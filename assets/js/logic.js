43578643c56f50c1ab2ff7c16a6995de9dddfbe8
Justus B <justus.c.brown@gmail.com>
2022-07-01 12:42:54 -0700

    modal on button click

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,21 @@
 
 
-
-var dateHeaderEl = $(".date-time") //get heaer element and change the value to today's date on last refresh
+// TODO: get new date on every second
+var dateHeaderEl = $(".date-time") //get header element and change the value to today's date on last refresh
 dateHeaderEl.text(moment().format("MMM Do, YYYY"))
+
+
+// open modal dialog box
+    //listen for click on add project
+    //open dialog on click
+
+//populate dialog with questions
+    //on sumbit save answers as varables
+    //save them to local storage
+    //render from local storage to lis 
+
+//listen for delete button click
+    //remove clicked li from page
+    //clear clicked li from localStorage
+
+
